a8290b9102eb34429e64c4aeae1cdd0221034a8b
Gerome Matilla <gerome.matilla07@gmail.com>
2020-08-31 13:22:50 +0800

    add function description

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -9,19 +9,19 @@
 # ░▀░▀░▀▀▀░▀▀▀░▀░░░▀▀▀░▀░▀░▀▀▀
 
 # Is git dirty?
-function _git_dirty
+function _git_dirty -d 'Checks if git is dirty'
 	echo (command git status -s --ignore-submodules=dirty ^/dev/null)
 end
 
 # Is PWD a git directory?
-function _git_directory
+function _git_directory -d 'Checks if $PWD is a git repository'
 	if git rev-parse --git-dir > /dev/null 2>&1
 		echo 0
 	end
 end
 
 # Set git status color
-function _git_status
+function _git_status -d 'Returns color based on the previous command status'
 	if [ (_git_directory) ]
 		# Check if dirty
 		if [ (_git_dirty) ]
@@ -36,7 +36,7 @@ function _git_status
 end
 
 # Set time background color
-function _time_bg
+function _time_bg -d 'Returns background color based on time'
 	set hour (date +%H)
 	if test $hour -ge 6 && test $hour -lt 12
 		echo brblue
@@ -48,7 +48,7 @@ function _time_bg
 end
 
 # Set time foreground color
-function _time_fg
+function _time_fg -d 'Returns foreground color based on time'
 	set hour (date +%H)
 	if test $hour -ge 6 && test $hour -lt 12
 		echo black
@@ -60,13 +60,13 @@ function _time_fg
 end
 
 # OS type
-function _os_type
+function _os_type -d 'Returns OS type'
 	set os_type ($SHELL -c "echo \$OSTYPE")
 	echo $os_type
 end
 
 # Distro name
-function _distro_name
+function _distro_name -d 'Returns linux distribution name'
 	set distro_name
 	if test -z $distro_name && test -r "/etc/os-release"
 		set distro_name (awk -F '=' '$1=="ID" { print $2 ;}' "/etc/os-release")
@@ -85,45 +85,45 @@ function _distro_name
 end
 
 # Distro icon
-function _distro_icon
+function _distro_icon -d 'Returns linux distribution icon'
 	switch (_distro_name)
-		case '*arch*';												set icon ''
-		case '*debian*';											set icon ''
-		case '*ubuntu*';											set icon ''
-		case '*raspbian*';										set icon ''
-		case '*mint*';												set icon ''
-		case '*manjaro*';										set icon ''
-		case '*elementary*';									set icon ''
-		case '*fedora*';											set icon ''
-		case '*coreos*';											set icon ''
-		case '*gentoo*';											set icon ''
-		case '*centos*';											set icon ''
-		case '*mageia*';											set icon ''
-		case '*opensuse*' '*tumbleweed*';		set icon ''
-		case '*sabayon*';										set icon ''
-		case '*slackware*';									set icon ''
-		case '*alpine*';											set icon ''
-		case '*devuan*';										set icon ''
-		case '*redhat*';											set icon ''
-		case '*aosc*';												set icon ''
-		case '*nixos*';											set icon ''
-		case '*void*';												set icon ''
-		case '*artix*';												set icon ''
-		case '*';														set icon ''
+		case '*arch*';				set icon ''
+		case '*debian*';				set icon ''
+		case '*ubuntu*';				set icon ''
+		case '*raspbian*';				set icon ''
+		case '*mint*';				set icon ''
+		case '*manjaro*';				set icon ''
+		case '*elementary*';				set icon ''
+		case '*fedora*';				set icon ''
+		case '*coreos*';				set icon ''
+		case '*gentoo*';				set icon ''
+		case '*centos*';				set icon ''
+		case '*mageia*';				set icon ''
+		case '*opensuse*' '*tumbleweed*';				set icon ''
+		case '*sabayon*';				set icon ''
+		case '*slackware*';				set icon ''
+		case '*alpine*';				set icon ''
+		case '*devuan*';				set icon ''
+		case '*redhat*';				set icon ''
+		case '*aosc*';				set icon ''
+		case '*nixos*';				set icon ''
+		case '*void*';				set icon ''
+		case '*artix*';				set icon ''
+		case '*';				set icon ''
 	end
 	echo $icon
 end
 
 # OS icon
-function _os_icon
+function _os_icon -d 'Returns OS icon'
 	# Icons sauce: https://nerdfonts.com/cheat-sheet
 	switch (_os_type)
-		case linux-gnu;											set icon (_distro_icon)
-		case darwin;												set icon ''
-		case CYGWIN_NT-'*' MSYS_NT-'*';		set icon ''
-		case freebsd openbsd dragonfly;			set icon ''
-		case sunos;												set icon ''
-		case '*';														set icon ''
+		case linux-gnu;				set icon (_distro_icon)
+		case darwin;				set icon ''
+		case CYGWIN_NT-'*' MSYS_NT-'*';				set icon ''
+		case freebsd openbsd dragonfly;				set icon ''
+		case sunos;				set icon ''
+		case '*';				set icon ''
 	end
 	echo $icon
 end
@@ -133,7 +133,7 @@ end
 # ░▀░░░▀░▀░▀▀▀░▀░▀░▀░░░░▀░░▀▀▀
 
 # SSH Prompt
-function _ssh_prompt
+function _ssh_prompt -d 'Returns SSH display'
 	set prompt
 	if set -q SSH_TTY
 		set prompt (set_color -b bryellow -o black)' SSH '
@@ -142,19 +142,19 @@ function _ssh_prompt
 end
 
 # Distro prompt
-function _distro_prompt
+function _distro_prompt -d 'Returns distro display'
 	set prompt_distro (set_color -b blue white)' '(_os_icon)' '
 	echo $prompt_distro
 end
 
 # Time prompt
-function _time_prompt
+function _time_prompt -d 'Returns time display'
 	set prompt_time (set_color -b (_time_bg) -o (_time_fg))' '(date +%H:%M)' '
 	echo $prompt_time
 end
 
 # user@host prompt
-function _user_host_prompt
+function _user_host_prompt -d 'Returns username and hostname display'
 	set -l user_hostname $USER@(prompt_hostname)
 	if [ $USER = 'root' ]
 		set user_bg red
@@ -175,7 +175,7 @@ function _user_host_prompt
 end
 
 # PWD prompt
-function _pwd_prompt
+function _pwd_prompt -d 'Returns PWD display'
 	# Check working directory if writable
 	if test -w $PWD
 		set pwd_color (_git_status)
@@ -187,7 +187,7 @@ function _pwd_prompt
 end
 
 # Status prompt
-function _status_prompt
+function _status_prompt -d 'Returns status display'
 	if not test $prev_status -eq 0
 		set_color $fish_color_error
 		echo -n (set_color -b red yellow) '✘ '
@@ -197,7 +197,7 @@ function _status_prompt
 	set_color normal
 end
 
-function _git_prompt
+function _git_prompt -d 'Returns Git display'
 	if [ (__fish_git_prompt) ]
 		set git_bg (_git_status)
 		set prompt (__fish_git_prompt) ' '
@@ -208,7 +208,7 @@ function _git_prompt
 	echo (set_color -b $git_bg -o black) $prompt
 end
 
-function _private_prompt
+function _private_prompt -d 'Returns private mode display'
 	if  not test -z $fish_private_mode
 		set prompt (set_color -b black white) '﫸'
 	else
@@ -222,7 +222,7 @@ end
 # ░▀▀▀░▀▀▀░▀░░░░▀░░░░░░▀░▀░▀░▀░▀░▀░▀▀░░░░▀░░░▀░▀░▀▀▀░▀░▀░▀░░░░▀░
 
 # Left-hand prompt
-function fish_prompt
+function fish_prompt -d 'Left-hand prompt'
 	set -g prev_status $status
 
 	# Window title
@@ -242,7 +242,7 @@ end
 # ░▀░▀░▀▀▀░▀▀▀░▀░▀░░▀░░░░░░▀░▀░▀░▀░▀░▀░▀▀░░░░▀░░░▀░▀░▀▀▀░▀░▀░▀░░░░▀░
 
 # Right-hand prompt
-function fish_right_prompt
+function fish_right_prompt -d 'Right-hand prompt'
 	printf '%s%s%s%s%s' (_status_prompt) (_git_prompt) (_time_prompt) (_private_prompt)
 	set_color normal
 end
